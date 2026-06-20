5ff5c2a2eec06f2eaeed73ddb160cb534c968c86
avycado13 <108358183+avycado13@users.noreply.github.com>
2026-06-20 07:46:39 -0700

    wwwwwwwalll jump!!!!!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
-
-
 const JUMP_VELOCITY = -325
+const WALL_JUMP_X = 180
 
 var dir := -1
 
@@ -11,16 +9,28 @@ var dir := -1
 @onready var tile_map_layer: TileMapLayer = $"../TileMapLayer"
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
-		velocity.y +=600 * delta
+		velocity.y += 600 * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Normal jump
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		# Wall jump
+		elif is_on_wall():
+			velocity.y = JUMP_VELOCITY
+
+			# Push away from the wall
+			if get_wall_normal().x > 0:
+				# Wall is on left
+				velocity.x = WALL_JUMP_X
+			else:
+				# Wall is on right
+				velocity.x = -WALL_JUMP_X
+
+	# Horizontal movement
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -120
 		sprite.flip_h = false
@@ -30,18 +40,20 @@ func _physics_process(delta: float) -> void:
 		sprite.flip_h = true
 		dir = 1
 	else:
-		velocity.x = 0
-		
+		# Don't stop horizontal movement immediately while wall jumping
+		if is_on_floor():
+			velocity.x = 0
+
+	if is_on_wall() and !is_on_floor() and velocity.y > 50:
+		velocity.y = 50
+	# Mining
 	if Input.is_action_just_pressed("ui_accept"):
 		if Input.is_action_pressed("ui_down"):
-			tile_map_layer.destroy_tile(position+Vector2(0,16))
+			tile_map_layer.destroy_tile(position + Vector2(0, 16))
 			velocity.y = 70
 		elif Input.is_action_pressed("ui_up"):
-			tile_map_layer.destroy_tile(position-Vector2(0,16))
+			tile_map_layer.destroy_tile(position - Vector2(0, 16))
 		else:
-			tile_map_layer.destroy_tile(position+ dir*Vector2(16,0))
-		
-		
-	
+			tile_map_layer.destroy_tile(position + dir * Vector2(16, 0))
 
 	move_and_slide()
