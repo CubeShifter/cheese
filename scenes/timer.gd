e09f372b713084c679dbe8ed31265ca383813951
avycado13 <108358183+avycado13@users.noreply.github.com>
2026-06-21 09:43:00 -0700

    timer pt1

--- scenes/timer.gd
+++ scenes/timer.gd
@@ -2,11 +2,9 @@ extends Timer
 
 @onready var rich_text_label: RichTextLabel = $"../CanvasLayer3/TextureRect2/RichTextLabel"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	rich_text_label.text = "[center]" + str(int(time_left))
+func _process(_delta: float) -> void:
+	var seconds := int(time_left)
+	var minutes := seconds / 60
+	seconds %= 60
+	
+	rich_text_label.text = "[center]%d:%02d[/center]" % [minutes, seconds]
