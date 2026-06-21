39dfc214cc5787fea733b908b93dd424c493f730
avycado13 <108358183+avycado13@users.noreply.github.com>
2026-06-21 11:27:30 -0700

    bbbbbbbbombs

--- scenes/timer.gd
+++ scenes/timer.gd
@@ -11,8 +11,3 @@ func _process(_delta: float) -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	wait_time = 120 + 15 * Global.upgrades["Time"]
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	rich_text_label.text = "[center]" + str(int(time_left))
